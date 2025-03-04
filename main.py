9c0abc27f439442b5ca66f4f0dacf7bde4c6afe1
NULL <kamwlung@gmail.com>
2025-03-04 16:19:33 +0000

    x

--- main.py
+++ main.py
@@ -7,72 +7,69 @@ import datetime
 
 @register("X", "SonyDog", "", "", "")
 class Main(Star):
-    def __init__(self, context: Context):
+    def init(self, context: Context):
         super().__init__(context)
-    
+
     @filter.command("epic")
     async def epic_free_game(self, message: AstrMessageEvent):
         '''EPIC 喜加一'''
         url = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions"
-
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 if resp.status != 200:
                     return CommandResult().error("请求失败")
                 data = await resp.json()
-
+        
         games = []
         upcoming = []
-
         for game in data["data"]["Catalog"]["searchStore"]["elements"]:
             title = game.get("title", "未知")
+            url_slug = game.get("urlSlug", "未知")
+            game_url = f"https://store.epicgames.com/zh-CN/p/{url_slug}"
+            
             try:
                 if not game.get("promotions"):
                     continue
-                original_price = game["price"]["totalPrice"]["fmtPrice"][
-                    "originalPrice"
-                ]
-                discount_price = game["price"]["totalPrice"]["fmtPrice"][
-                    "discountPrice"
-                ]
+                
+                original_price = game["price"]["totalPrice"]["fmtPrice"]["originalPrice"]
+                discount_price = game["price"]["totalPrice"]["fmtPrice"]["discountPrice"]
                 promotions = game["promotions"]["promotionalOffers"]
                 upcoming_promotions = game["promotions"]["upcomingPromotionalOffers"]
-
+                
                 if promotions:
                     promotion = promotions[0]["promotionalOffers"][0]
                 else:
                     promotion = upcoming_promotions[0]["promotionalOffers"][0]
+                
                 start = promotion["startDate"]
                 end = promotion["endDate"]
+                
                 # 2024-09-19T15:00:00.000Z
-                start_utc8 = datetime.datetime.strptime(
-                    start, "%Y-%m-%dT%H:%M:%S.%fZ"
-                ) + datetime.timedelta(hours=8)
+                start_utc8 = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ") + datetime.timedelta(hours=8)
                 start_human = start_utc8.strftime("%Y-%m-%d %H:%M")
-                end_utc8 = datetime.datetime.strptime(
-                    end, "%Y-%m-%dT%H:%M:%S.%fZ"
-                ) + datetime.timedelta(hours=8)
+                end_utc8 = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S.%fZ") + datetime.timedelta(hours=8)
                 end_human = end_utc8.strftime("%Y-%m-%d %H:%M")
+                
                 discount = float(promotion["discountSetting"]["discountPercentage"])
                 if discount != 0:
                     # 过滤掉不是免费的游戏
                     continue
-
+                
                 if promotions:
                     games.append(
-                        f"【{title}】\n原价: {original_price} | 现价: {discount_price}\n活动时间: {start_human} - {end_human}"
+                        f"【{title}】\n原价: {original_price} | 现价: {discount_price}\n活动时间: {start_human} - {end_human}\n游戏链接: {game_url}"
                     )
                 else:
                     upcoming.append(
-                        f"【{title}】\n原价: {original_price} | 现价: {discount_price}\n活动时间: {start_human} - {end_human}"
+                        f"【{title}】\n原价: {original_price} | 现价: {discount_price}\n活动时间: {start_human} - {end_human}\n游戏链接: {game_url}"
                     )
-
             except BaseException as e:
-                raise e
+                logger.error(f"处理 {title} 时出现错误: {e}")
                 games.append(f"处理 {title} 时出现错误")
-
+        
         if len(games) == 0:
             return CommandResult().message("暂无免费游戏")
+        
         return (
             CommandResult()
             .message(
